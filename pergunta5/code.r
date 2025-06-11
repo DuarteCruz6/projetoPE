b1b173f2eeedfb8b7885e9df6ff3d739d5f1c844
Duarte Cruz <dudu.cruz2005@gmail.com>
2025-06-11 17:02:10 +0100

    q5

--- pergunta5/code.r
+++ pergunta5/code.r
@@ -4,16 +4,31 @@ set.seed(1084)
 #dimensão da amostra
 n <- 39000
 
-#simular n jogadas: cada jogada é a soma de 3 dados (uniformes de 1 a 6)
-dados <- sample(1:6, 3 * n, replace = TRUE)
-somas <- rowSums(matrix(dados, nrow = n, ncol = 3))
+somas_9 <- 0
+somas_10 <- 0
+
+#simulação
+for (i in 1:n) {
+  #lançar 3 dados
+  dados <- sample(1:6, 3, replace = TRUE)
+  
+  #soma
+  soma_atual <- sum(dados)
+  
+  #contar somas
+  if (soma_atual == 9) {
+    somas_9 <- somas_9 + 1
+  } else if (soma_atual == 10) {
+    somas_10 <- somas_10 + 1
+  }
+}
 
 #calcular frequências relativas
-freq_9 <- sum(somas == 9) / n
-freq_10 <- sum(somas == 10) / n
+freq_rel_9 <- somas_9 / n
+freq_rel_10 <- somas_10 / n
 
 #diferença entre frequências relativas
-diff_freq <- freq_10 - freq_9
+diferenca_freq <- freq_rel_10 - freq_rel_9
 
 #arredondar a 4 casas decimais
-round(diff_freq, 4)
+print(round(diferenca_freq, 4))
